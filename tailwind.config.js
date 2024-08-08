6a0c893e5110c57d562ed4e875264e2127636313
Bernabe Posso <bernabeposs@gmail.com>
2024-08-08 11:00:19 -0500

    Tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '760px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: "#3D8AFF",
+      secondary: "#4A5A7A",
+      background: "#0D1117",
+      secondaryBg: "#2A3138",
+      text: "#E6E8EA",
+      elements: "#6A9AE6",
+    },
+    fontFamily: {
+      mont: ["var(--font-montserrat)", "sans-serif"],
+      pop: ["var(--font-poppins)", "sans-serif"],
+    },
     extend: {
-      colors: {
-        primary: "#3D8AFF",
-        secondary: "#4A5A7A",
-        background: "#0D1117",
-        secondaryBackground: "#2A3138",
-        text: "#E6E8EA",
-        interactiveElements: "#6A9AE6",
-      },
-      fontFamily: {
-        mont: ["var(--font-montserrat)", "sans-serif"],
-        pop: ["var(--font-poppins)", "sans-serif"],
-      },
     },
   },
   plugins: [],
