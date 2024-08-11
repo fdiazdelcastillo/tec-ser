1727cc5302f4bc2c76cf502f92e686b403f5ea01
Fernando Delfin Diaz del Castillo Pabon <fdiazp@Ferandos-MacBook-Pro.local>
2024-08-11 17:49:57 -0500

    Testimonials Component Update

--- styles/Fonts.ts
+++ styles/Fonts.ts
@@ -3,11 +3,11 @@ import { Montserrat, Poppins } from "next/font/google";
 export const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["400", "600", "700"],
 });
 
 export const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["400", "600", "700"],
 });
