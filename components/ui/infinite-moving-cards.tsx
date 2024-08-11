41c6ec96d61d8269d386f6c5c7adf84d8ead98ab
Fernando Delfin Diaz del Castillo Pabon <fdiazp@Ferandos-MacBook-Pro.local>
2024-08-11 16:49:33 -0500

    Testimonials Component Update

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
   items,
@@ -23,9 +23,11 @@ export const InfiniteMovingCards = ({
   const containerRef = React.useRef<HTMLDivElement>(null);
   const scrollerRef = React.useRef<HTMLUListElement>(null);
 
+  useEffect(() => {
+    addAnimation();
+  }, []);
   const [start, setStart] = useState(false);
-
-  const addAnimation = useCallback(() => {
+  function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
       const scrollerContent = Array.from(scrollerRef.current.children);
 
@@ -40,10 +42,8 @@ export const InfiniteMovingCards = ({
       getSpeed();
       setStart(true);
     }
-    // }, [direction, speed]);
-  }, []);
-
-  const getDirection = useCallback(() => {
+  }
+  const getDirection = () => {
     if (containerRef.current) {
       if (direction === "left") {
         containerRef.current.style.setProperty(
@@ -57,9 +57,8 @@ export const InfiniteMovingCards = ({
         );
       }
     }
-  }, [direction]);
-
-  const getSpeed = useCallback(() => {
+  };
+  const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
         containerRef.current.style.setProperty("--animation-duration", "20s");
@@ -69,12 +68,7 @@ export const InfiniteMovingCards = ({
         containerRef.current.style.setProperty("--animation-duration", "80s");
       }
     }
-  }, [speed]);
-
-  useEffect(() => {
-    addAnimation();
-  }, [addAnimation]);
-
+  };
   return (
     <div
       ref={containerRef}
@@ -105,15 +99,15 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-base leading-[1.6] text-text font-normal">
+              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-[20px] leading-[1.6] text-elements font-mont font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
                     {item.name}
                   </span>
-                  <span className=" text-sm leading-[1.6] text-elements font-mont font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
                     {item.title}
                   </span>
                 </span>
