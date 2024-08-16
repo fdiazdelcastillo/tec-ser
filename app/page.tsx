95b7ecd9ecd062f2b298ad9eba387cc654894e4a
Fernando Delfin Diaz del Castillo Pabon <fdiazp@Ferandos-MacBook-Pro.local>
2024-08-16 15:32:49 -0500

    comparison table

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ import { Features } from "@/components/Features";
 import { Testimonials } from "@/components/Testimonials";
 import { Faq } from "@/components/Faq";
 import { ContactInfo } from "@/components/ContactInfo";
+import ComparisonTable from "@/components/ComparisonTable";
+import Check from "@/public/Check.svg";
+
 
 export default function Home() {
   return (
@@ -17,6 +20,112 @@ export default function Home() {
       <Hero />
       <Features />
       <Services />
+      <ComparisonTable
+        card={[
+          {
+            title: "Sitio Web",
+            precio: (
+              <>
+                <div>
+                  us$ <span className="font-bold text-xl font-mont">200</span>
+                  mes <br />
+                  us$ <span className="font-bold text-xl font-mont">160</span>
+                  mes
+                </div>
+              </>
+            ),
+            paginasIncluidas: "3",
+            despliegue: "1 TB/mes",
+            refrescoUi: "Anual",
+            iaChat: Check,
+          },
+          {
+            title: "Catálogo Web",
+            precio: (
+              <>
+                <div>
+                  us$ <span className="font-bold text-xl font-mont">400</span>
+                  mes <br />
+                  us$ <span className="font-bold text-xl font-mont">320</span>
+                  mes
+                </div>
+              </>
+            ),
+            paginasIncluidas: "6",
+            despliegue: "1 TB/mes",
+            refrescoUi: "Cada 2 años",
+            iaChat: Check,
+          },
+          {
+            title: "Tienda Web",
+            precio: (
+              <>
+                <div>
+                  us$ <span className="font-bold text-xl font-mont">600</span>
+                  mes <br />
+                  us$ <span className="font-bold text-xl font-mont">480</span>
+                  mes
+                </div>
+              </>
+            ),
+            paginasIncluidas: "11",
+            despliegue: "1 TB/mes",
+            refrescoUi: "Cada 2 años",
+            iaChat: Check,
+          },
+          {
+            title: "Tienda Movil",
+            precio: (
+              <>
+                <div>
+                  us$ <span className="font-bold text-xl font-mont">1000</span>
+                  mes <br />
+                  us$ <span className="font-bold text-xl font-mont">800</span>
+                  mes
+                </div>
+              </>
+            ),
+            paginasIncluidas: "8",
+            despliegue: "1 TB/mes",
+            refrescoUi: "Cada 2 años",
+            iaChat: Check,
+          },
+          {
+            title: "Tienda Web y Movil",
+            precio: (
+              <>
+                <div>
+                  us$ <span className="font-bold text-xl font-mont">1400</span>
+                  mes <br />
+                  us$ <span className="font-bold text-xl font-mont">1120</span>
+                  mes
+                </div>
+              </>
+            ),
+            paginasIncluidas: "12",
+            despliegue: "1 TB/mes",
+            refrescoUi: "Anual",
+            iaChat: Check,
+          },
+          {
+            title: "IA Chat",
+            precio: (
+              <>
+                <div>
+                  us$ <span className="font-bold text-xl font-mont">100</span>
+                  mes <br />
+                  us$ <span className="font-bold text-xl font-mont">80</span>
+                  mes
+                </div>
+              </>
+            ),
+            paginasIncluidas: "N/A",
+            despliegue: "N/A",
+            refrescoUi: "N/A",
+            iaChat: "",
+          },
+        ]}
+      />
       <Faq />
       <Testimonials />
       <ContactInfo/>
