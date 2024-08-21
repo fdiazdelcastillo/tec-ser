4d1485052ae433e229d8b21fbed940e3248c8e66
Fernando Delfin Diaz del Castillo Pabon <fdiazp@Ferandos-MacBook-Pro.local>
2024-08-21 13:15:13 -0500

    Contact Info Adjustments

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -4,52 +4,56 @@ import { SignupForm } from "./SignupForm";
 
 export const ContactInfo = () => {
   return (
-    <div className="flex justify-between">
+    <div className="block md:flex md:justify-between">
       <div className="mx-auto mt-2">
-        <h4 className="text-3xl text-text font-mont font-bold">Información de Contacto</h4>
-        <p className="text-text font-pop">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h4 className="text-3xl text-text font-mont font-bold">
+          Información de Contacto
+        </h4>
+        <p className="text-text font-pop">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
         <div className="flex items-center gap-x-4 mt-10">
-            <Image
-              src="/email.png"
-              width={30}
-              height={30}
-              alt="logo mail"
-              className="max-w-36 sm:max-w-full"
-            />
-            <p className="text-text font-pop text-xl">correo01@omnitreck.com</p>
+          <Image
+            src="/email.png"
+            width={30}
+            height={30}
+            alt="logo mail"
+            className="max-w-36 sm:max-w-full"
+          />
+          <p className="text-text font-pop text-xl">correo01@omnitreck.com</p>
         </div>
         <div className="flex items-center gap-x-4 mt-4">
-            <Image
-              src="/email.png"
-              width={30}
-              height={30}
-              alt="logo mail"
-              className="max-w-36 sm:max-w-full"
-            />
-            <p className="text-text font-pop text-xl">correo02@omnitreck.com</p>
+          <Image
+            src="/email.png"
+            width={30}
+            height={30}
+            alt="logo mail"
+            className="max-w-36 sm:max-w-full"
+          />
+          <p className="text-text font-pop text-xl">correo02@omnitreck.com</p>
         </div>
         <div className="flex items-center gap-x-4 mt-4">
-            <Image
-              src="/email.png"
-              width={30}
-              height={30}
-              alt="logo mail"
-              className="max-w-36 sm:max-w-full"
-            />
-            <p className="text-text font-pop text-xl">correo03@omnitreck.com</p>
+          <Image
+            src="/email.png"
+            width={30}
+            height={30}
+            alt="logo mail"
+            className="max-w-36 sm:max-w-full"
+          />
+          <p className="text-text font-pop text-xl">correo03@omnitreck.com</p>
         </div>
         <div className="flex items-center gap-x-4 mt-8">
-            <Image
-              src="/whatsapp.png"
-              width={30}
-              height={30}
-              alt="logo whatsapp"
-              className="max-w-36 sm:max-w-full"
-            />
-            <p className="text-text font-pop text-xl">+57 3524856241</p>
+          <Image
+            src="/whatsapp.png"
+            width={30}
+            height={30}
+            alt="logo whatsapp"
+            className="max-w-36 sm:max-w-full"
+          />
+          <p className="text-text font-pop text-xl">+57 3524856241</p>
         </div>
       </div>
-      <SignupForm/>;
+      <SignupForm />;
     </div>
   );
-}
+};
