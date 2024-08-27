26ba94904c83eabd063c61edfc4f699878217d25
Maria Fernanda Diaz Del Castillo Posso <mfdiazdelcastillo@gmail.com>
2024-08-27 12:05:41 -0500

    Component ComparisonTable reponsive updated

--- components/ComparisonTable.tsx
+++ components/ComparisonTable.tsx
@@ -15,7 +15,7 @@ export type ComparisonTableProps = {
 
 const ComparisonTableCard: Component<ComparisonTableCardProps> = (props) => {
   return (
-    <div className="flex flex-shrink-0 flex-col py-14 px-6 gap-7 text-text items-center font-mont font-semibold text-base max-md:text-sm border-l-2 border-background w-[187px] max-md:w-44 relative">
+    <div className="flex flex-shrink-0 flex-col py-14 px-6 max-sm:px-3 gap-7 max-md:gap-4 text-text items-center font-mont font-semibold text-base max-md:text-sm border-l-2 border-background xl:w-[195px] lg:w-[189px] max-md:w-[177px] relative">
       <div
         style={{
           backgroundImage: "url(/noise.webp)",
@@ -24,7 +24,7 @@ const ComparisonTableCard: Component<ComparisonTableCardProps> = (props) => {
         className="absolute object-contain inset-0 w-full h-full  scale-y-100 transform opacity-[0.019]"
       ></div>
       <div className="w-44 max-md:w-36 mb-[50px] h-8">
-        <div className="font-bold text-xl max-md:text-lg text-primary text-center">
+        <div className="font-bold text-xl max-md:text-lg sm:text-base text-primary text-center">
           {props.title}
         </div>
       </div>
@@ -32,7 +32,7 @@ const ComparisonTableCard: Component<ComparisonTableCardProps> = (props) => {
         <div className="font-light text-sm max-md:text-sm">{props.precio}</div>
       </div>
       <button className="py-[5px] px-3 bg-primary rounded-[50px] font-semibold text-[12px] text-center border-solid border-elements border-[4px] my-6">
-        Contactanos
+        Contáctanos
       </button>
       <div>{props.paginasIncluidas}</div>
       <div>{props.despliegue}</div>
@@ -63,8 +63,8 @@ const ComparisonTable: Component<ComparisonTableProps> = (props) => {
   }
 
   return (
-    <div className="flex flex-row justify-normal mx-8 max-sm:mx-5 flex-grow rounded-[20px] border-solid border-background border-2 [background-image:radial-gradient(53%_52%_at_top,rgba(255,255,255,0.15),rgba(255,255,255,0))] max-xl:overflow-x-scroll scroll-smooth overflow-hidden">
-      <div className="flex flex-col py-14 px-10 max-sm:px-8 gap-7 text-text font-mont text-base max-md:text-sm font-semibold [background-image:radial-gradient(143%_46%_at_bottom_left_,rgba(61,138,255,0.50),rgba(34,71,126,0.50),rgba(13,17,23,0.2))] max-lg:sticky -left-1 sm:sticky max-xl:bg-background z-[1]">
+    <div className="flex flex-row flex-grow rounded-[20px] border-solid border-background border-2 [background-image:radial-gradient(53%_52%_at_top,rgba(255,255,255,0.15),rgba(255,255,255,0))] max-xl:overflow-x-scroll scroll-smooth overflow-hidden">
+      <div className="flex flex-col py-14 px-10 max-sm:px-6 gap-7 max-md:gap-4 text-text font-mont text-base max-md:text-sm font-semibold [background-image:radial-gradient(143%_46%_at_bottom_left_,rgba(61,138,255,0.50),rgba(34,71,126,0.50),rgba(13,17,23,0.2))] max-lg:sticky -left-1 sm:sticky max-xl:bg-background z-[1]">
         <div
           style={{
             backgroundImage: "url(/noise.webp)",
@@ -72,7 +72,7 @@ const ComparisonTable: Component<ComparisonTableProps> = (props) => {
           }}
           className="absolute object-contain inset-0 w-full h-full  scale-y-100 transform opacity-[0.019]"
         ></div>
-        <div className="mb-12 text-center text-xl max-md:text-lg text-primary font-bold ">
+        <div className="mb-12 text-center text-xl max-md:text-lg sm:text-base text-primary font-bold ">
           Características
         </div>
         <div className="mb-[148px] text-2xl max-md:text-xl">Precio</div>
@@ -81,7 +81,7 @@ const ComparisonTable: Component<ComparisonTableProps> = (props) => {
         <div>Refresco UI</div>
         <div>IA Chat</div>
       </div>
-      <div className="flex flex-row  ">{cardItems}</div>
+      <div className="flex flex-row">{cardItems}</div>
     </div>
   );
 };
