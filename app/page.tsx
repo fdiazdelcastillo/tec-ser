3558d26a589cad657e8638055d0ca05daefed48d
Fernando Delfin Diaz del Castillo Pabon <fdiazp@Ferandos-MacBook-Pro.local>
2024-08-29 16:18:57 -0500

    email test

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { ContactInfo } from "@/components/ContactInfo";
 import ComparisonTable from "@/components/ComparisonTable";
 import Check from "@/public/Check.svg";
 import Footer from "@/components/Footer";
-import { ContactForm } from "@/components/ContactForm";
+import { FeedbackForm } from "@/components/FeedbackForm";
 
 export default function Home() {
   return (
@@ -130,7 +130,7 @@ export default function Home() {
       <Faq />
       <Testimonials />
       <ContactInfo />
-      <ContactForm />
+      <FeedbackForm />
       <Footer />
     </main>
   );
