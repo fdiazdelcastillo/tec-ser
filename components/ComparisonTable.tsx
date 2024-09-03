8957ff22dbc69b0c0957b5e5d0e99c39d2a015ef
Maria Fernanda Diaz Del Castillo Posso <mfdiazdelcastillo@gmail.com>
2024-09-03 09:42:20 -0500

    Component ComparisonTable reponsive actualized

--- components/ComparisonTable.tsx
+++ components/ComparisonTable.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { div } from "three/webgpu";
 
 type ComparisonTableCardProps = {
   title: string;
@@ -15,7 +16,7 @@ export type ComparisonTableProps = {
 
 const ComparisonTableCard: Component<ComparisonTableCardProps> = (props) => {
   return (
-    <div className="flex flex-shrink-0 flex-col py-14 px-6 max-sm:px-3 gap-7 max-md:gap-4 text-text items-center font-mont font-semibold text-base max-md:text-sm border-l-2 border-background xl:w-[195px] lg:w-[189px] max-md:w-[177px] relative">
+    <div className="flex flex-shrink-0 flex-col py-14 md:px-6 max-sm:px-3 lg:gap-7 md:gap-4 max-sm:gap-3 text-text items-center font-mont font-semibold lg:text-base max-md:text-sm border-l-2 border-background relative xl:w-1/6 lg:w-[20.7%] md:w-[23.2%] max-sm:w-1/2 ">
       <div
         style={{
           backgroundImage: "url(/noise.webp)",
@@ -23,15 +24,15 @@ const ComparisonTableCard: Component<ComparisonTableCardProps> = (props) => {
         }}
         className="absolute object-contain inset-0 w-full h-full  scale-y-100 transform opacity-[0.019]"
       ></div>
-      <div className="w-44 max-md:w-36 mb-[50px] h-8">
-        <div className="font-bold text-xl max-md:text-lg sm:text-base text-primary text-center">
+      <div className="h-12 mb-8">
+        <div className="font-bold lg:text-xl md:text-lg max-sm:text-base  text-primary text-center">
           {props.title}
         </div>
       </div>
       <div className="flex flex-col items-start">
-        <div className="font-light text-sm max-md:text-sm">{props.precio}</div>
+        <div className="font-light">{props.precio}</div>
       </div>
-      <button className="py-[5px] px-3 bg-primary rounded-[50px] font-semibold text-[12px] text-center border-solid border-elements border-[4px] my-6">
+      <button className="py-[5px] px-2 bg-primary rounded-[50px] font-semibold text-[12px] text-center border-solid border-elements border-[4px] my-6">
         Contáctanos
       </button>
       <div>{props.paginasIncluidas}</div>
@@ -63,8 +64,8 @@ const ComparisonTable: Component<ComparisonTableProps> = (props) => {
   }
 
   return (
-    <div className="flex flex-row flex-grow rounded-[20px] border-solid border-background border-2 [background-image:radial-gradient(53%_52%_at_top,rgba(255,255,255,0.15),rgba(255,255,255,0))] max-xl:overflow-x-scroll scroll-smooth overflow-hidden">
-      <div className="flex flex-col py-14 px-10 max-sm:px-6 gap-7 max-md:gap-4 text-text font-mont text-base max-md:text-sm font-semibold [background-image:radial-gradient(143%_46%_at_bottom_left_,rgba(61,138,255,0.50),rgba(34,71,126,0.50),rgba(13,17,23,0.2))] max-lg:sticky -left-1 sm:sticky max-xl:bg-background z-[1]">
+    <div className="flex flex-shrink-0 flex-row flex-grow rounded-[20px] border-solid border-background border-2 [background-image:radial-gradient(53%_52%_at_top,rgba(255,255,255,0.15),rgba(255,255,255,0))] max-xl:overflow-x-scroll scroll-smooth overflow-hidden ">
+      <div className="flex flex-col flex-shrink-0 py-14 md:px-10 max-sm:px-6 lg:gap-7 md:gap-4 max-sm:gap-3 text-text font-mont lg:text-base max-md:text-sm font-semibold [background-image:radial-gradient(143%_46%_at_bottom_left_,rgba(61,138,255,0.50),rgba(34,71,126,0.50),rgba(13,17,23,0.2))] max-lg:sticky -left-1 sm:sticky max-xl:bg-background z-[1] max-sm:w-[50%]">
         <div
           style={{
             backgroundImage: "url(/noise.webp)",
@@ -72,16 +73,18 @@ const ComparisonTable: Component<ComparisonTableProps> = (props) => {
           }}
           className="absolute object-contain inset-0 w-full h-full  scale-y-100 transform opacity-[0.019]"
         ></div>
-        <div className="mb-12 text-center text-xl max-md:text-lg sm:text-base text-primary font-bold ">
+        <div className="h-12 mb-8 text-center lg:text-xl md:text-lg max-sm:text-base text-primary font-bold ">
           Características
         </div>
-        <div className="mb-[148px] text-2xl max-md:text-xl">Precio</div>
+        <div className="lg:mb-[142px] md:mb-[128px] max-sm:mb-[130px] lg:text-2xl md:text-xl max-sm:text-lg">
+          Precio
+        </div>
         <div>Páginas incluidas</div>
         <div>Despliegue</div>
         <div>Refresco UI</div>
         <div>IA Chat</div>
       </div>
-      <div className="flex flex-row">{cardItems}</div>
+      <div className="flex flex-row flex-grow w-[100%]">{cardItems}</div>
     </div>
   );
 };
